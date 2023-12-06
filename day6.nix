1de69e9a6f1d267072d5d6c46dc6b3a921e9d58c
Chris LaRose <cjlarose@gmail.com>
2023-12-06 15:27:20 -0800

    Complete day 6 part 2

--- day6.nix
+++ day6.nix
@@ -16,24 +16,50 @@
       f = raceDurationMs: recordDistanceMm: { inherit raceDurationMs recordDistanceMm; };
     in lists.zipListsWith f times distances;
 
-    totalDistanceMm = raceDurationMs: buttonHoldMs: 
+    abs = x: if x < 0 then - x else x;
+    heronsMethod = (x: guess:
       let
-        movingMs = raceDurationMs - buttonHoldMs;
-        speedMmPerMs = buttonHoldMs;
+        nextGuess = (guess + x / guess) / 2;
       in
-        movingMs * speedMmPerMs;
+      if abs (guess - nextGuess) < 1.0e-6 then nextGuess else heronsMethod x nextGuess
+    );
+    sqrt = x: heronsMethod x 1.0;
 
-    possibleDistances = raceDurationMs:
-      map (totalDistanceMm raceDurationMs) (lists.range 0 raceDurationMs);
+    # distance = (duration - buttonHold) * buttonHold
+    # to find intersections with record, solve for buttonHold
+    # record = (c - x) * x
+    # 0 = (c - x) * x - record
+    # 0 = -x^2 + cx - record
+    # standard form
+    # 0 = -x^2 + bx + -c
+    # x = (-b +- sqrt(b^2 - 4ac)) / 2a
+    # x = (-b +- sqrt(b^2 - 4c)) / -2, where b is the race duration and c is the record
 
+    # if b = 7 and c = 9,
+    # x = (-7 +- sqrt(49 - 36)) / -2
+    # x = (-7 +- sqrt(13)) / -2
+    # x = 5.3, x = 1.7
+    # integral solutions exist where x>= 2 and x <= 5, so the total number of wins in 5 - 2 + 1 = 4
     numberOfPossibleWins = { raceDurationMs, recordDistanceMm }:
-      builtins.length (builtins.filter (x: x > recordDistanceMm) (possibleDistances raceDurationMs));
+      let
+        discriminant = sqrt (raceDurationMs * raceDurationMs - 4 * recordDistanceMm);
+        maxWinningButtonHoldDuration = (raceDurationMs + discriminant) / 2;
+        minWinningButtonHoldDuration = (raceDurationMs - discriminant) / 2;
+      in builtins.floor maxWinningButtonHoldDuration - builtins.ceil minWinningButtonHoldDuration + 1;
 
     numPossibleWinsForEachRace = map numberOfPossibleWins races;
     product = builtins.foldl' (acc: x: acc * x) 1;
 
+    bigRace = let
+      concatAsStrings = nums: strings.toInt (strings.concatStrings (map builtins.toString nums));
+    in {
+      raceDurationMs = concatAsStrings times;
+      recordDistanceMm = concatAsStrings distances;
+    };
+    numPossibleWinsForBigRace = numberOfPossibleWins bigRace;
+
     solution = pkgs.writeTextFile {
       name = "day6";
-      text = "${builtins.toString (product numPossibleWinsForEachRace)}\n";
+      text = "${builtins.toString (product numPossibleWinsForEachRace)}\n${builtins.toString numPossibleWinsForBigRace}\n";
     };
   }
